734b7079ffa6444fed8491f91088bd45900a25a5
diego <you@example.com>
2022-01-13 01:30:59 -0500

    Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,4 +25,14 @@ export class AppComponent {
     this.person.age += 1;
   }
 
+  onScroll(event: Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    console.log(element);
+    this.person.name = element.value;
+  }
 }
