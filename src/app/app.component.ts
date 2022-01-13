56f2f379d622b4d26b48af3851fc9a03c79bcd43
diego <you@example.com>
2022-01-12 23:37:10 -0500

    String interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Diego';
+  age = 25;
+  img =  'https://source.unsplash.com/random';
 }
