06021a56c183a86e159d44cae2016cc3b1e61503
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-04-14 21:58:34 -0500

    feat: Reactividad basica, BehavioSubject Rxjs

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   activeMenu = false;
-
-  constructor() { }
+  counter = 0;
+  constructor(
+    private storeService: StoreService 
+  ) { }
 
   ngOnInit(): void {
+    this.storeService.myCart$.subscribe(products => {
+      console.log('products',products);
+      this.counter = products.length;
+    }); 
   }
 
   toggleMenu(){
--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../product.model';
-
+import { BehaviorSubject } from 'rxjs'; //state Management Store. crea Observable para que otros componentes se puedan suscribir apenas tenga un cambio
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
-  private myShopingCart: Product [] = [];
+  private myShopingCart: Product [] = []; //List current shopping
+
+  /* Estado compartido */
+  private myCart = new BehaviorSubject<Product[]>([]);//array de produts state initial vacio ([])
+  //Observable
+  myCart$ = this.myCart.asObservable(); 
 
   constructor() { }
 
   addProduct(product: Product){
     this.myShopingCart.push(product);
+    this.myCart.next(this.myShopingCart); 
   }
 
   //metodo para exponer el shoppingCart
